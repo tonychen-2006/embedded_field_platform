a68e9e9843532903381f6617e14d005dc25bf2f5
Tony Chen <tonychengangyi@gmail.com>
2026-06-09 13:02:02 -0700

    Adjusted baudrate.

--- components/Core/Src/main.c
+++ components/Core/Src/main.c
@@ -125,8 +125,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
+  MX_ADC1_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   rtc_test_passed = RTC_TestRunOnce() ? 1U : 0U;
   GPS_Init();
@@ -333,7 +335,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
